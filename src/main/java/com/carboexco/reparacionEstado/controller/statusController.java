498427d22f794aa7d1bdea9764a44e0e39d2386f
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-03-03 13:34:06 -0500

    controller de estado

---
+++ src/main/java/com/carboexco/reparacionEstado/controller/statusController.java
@@ -0,0 +1,14 @@
+package com.carboexco.reparacionEstado.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class statusController {
+    @GetMapping({"/status"})
+    public String status(){
+        return "ok";
+    }
+}
