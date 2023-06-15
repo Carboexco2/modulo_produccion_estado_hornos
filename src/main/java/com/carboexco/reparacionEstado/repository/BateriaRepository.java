c004071c20675f29aee9c1a36831639e15b62c39
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-06-15 16:23:38 -0500

    actualizar repositorio

--- src/main/java/com/carboexco/reparacionEstado/repository/BateriaRepository.java
+++ src/main/java/com/carboexco/reparacionEstado/repository/BateriaRepository.java
@@ -4,5 +4,10 @@ import com.carboexco.reparacionEstado.entity.Bateria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface BateriaRepository extends JpaRepository<Bateria, Integer>, JpaSpecificationExecutor<Bateria> {
+    List<Bateria> findByIdChimenea_IdOrderByNombreAsc(Integer id);
+
+
 }
